1fd10955a57a81ac1addb681dae50ca0e412f882
NithishKumar-coder <54560557+NithishKumar-coder@users.noreply.github.com>
2019-12-04 20:12:29 +0530

    Update sum.py

--- sum.py
+++ sum.py
@@ -1,6 +1,9 @@
-S=int(input())
-y=S
-while(S>0):
-    y=(S-1)+y
-    S=S-1
-print(y)
+try:
+  S=int(input())
+  y=S
+  while(S>0):
+      y=(S-1)+y
+      S=S-1
+  print(y)
+except:
+  print('invalid')
